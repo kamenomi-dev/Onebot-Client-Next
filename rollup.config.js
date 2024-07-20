12878d676f053cb5a7d17027f9e6d9daddd86aec
kamenomi-dev <2937396379@qq.com>
2024-07-21 01:31:05 +0800

    v0.0.4

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,16 @@
 import typescript from "@rollup/plugin-typescript";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-import pkg from "./package.json";
 
 export default {
   input: "./src/index.ts",
   output: [
     {
-      file: pkg.main,
+      file: "lib/iondex.js",
       format: "cjs",
     },
     {
-      file: pkg.module,
+      file: "lib/index.esm.js",
       format: "esm",
     },
   ],
